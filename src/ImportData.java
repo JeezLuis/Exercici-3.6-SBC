dfd00ff63e824f454ec6d85df8e2d0688adc3a69
Roger Casas <roger.casas@students.salle.url.edu>
2021-05-24 18:59:15 +0200

    pepega

--- src/ImportData.java
+++ src/ImportData.java
@@ -13,7 +13,7 @@ import java.util.ArrayList;
 public class ImportData {
 
 
-
+    //N-word
     public ImportData(String path) {
         Gson gson = new Gson();
         JsonObject allData = null;
