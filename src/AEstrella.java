823e977492db3b5d7be92b279a62cb0abb8a61c7
Lluis Farre Castan <luisfarrecastan@gmail.com>
2021-05-24 19:56:12 +0200

    Update AEstrella.java

--- src/AEstrella.java
+++ src/AEstrella.java
@@ -1,16 +1,34 @@
+import Model.City;
 import Model.Graf;
+
+import java.util.ArrayList;
 
 public class AEstrella {
 
-    private Graf graf;
-    private String from;
-    private String to;
-
     public AEstrella(Graf graf, String from, String to) {
-        this.graf = graf;
-        this.from = from;
-        this.to = to;
+        boolean trobat = false;
 
-        
+        City ciutat_origen = getNodeFromGraf(graf, from);
+        City ciutat_desti = getNodeFromGraf(graf, to);
+
+        ArrayList<City> senseVisitar = new ArrayList<>(graf.getAdjacents(ciutat_origen));
+
+        while (senseVisitar.size() > 0){
+            City current = senseVisitar.get(0);
+            if (current == ciutat_desti){
+                trobat = true;
+                break;
+            }
+            else{
+                senseVisitar.remove(current);
+            }
+        }
+    }
+
+    private City getNodeFromGraf(Graf graf, String name){
+        for (City node: graf.getNodes()) {
+            if(node.getName().equals(name)) return node;
+        }
+        return null;
     }
 }
